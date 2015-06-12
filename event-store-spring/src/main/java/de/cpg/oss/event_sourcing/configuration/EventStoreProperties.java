fc7feabebd87d702cace0c0e4f2600cc2422eb14
Forstner Michael <michael.forstner@cpg.de>
2015-06-12 19:54:17 +0200

    Enable spring-boot-autoconfiguration feature

---
+++ event-store-spring/src/main/java/de/cpg/oss/event_sourcing/configuration/EventStoreProperties.java
@@ -0,0 +1,19 @@
+package de.cpg.oss.event_sourcing.configuration;
+
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+@Data
+@ConfigurationProperties(prefix = EventStoreAutoConfiguration.CONFIG_PREFIX, ignoreUnknownFields = true)
+public class EventStoreProperties {
+
+    private String username = "admin";
+
+    private String password = "changeit";
+
+    private String hostname = "localhost";
+
+    private int port = 1113;
+
+    private int maxEventsToLoad = 100;
+}
